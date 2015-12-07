a008036f867ed6bef01290c0dbc938e17a01eed5
Piotr Pradzynski <prondzyn@gmail.com>
2015-12-07 18:44:26 +0100

    #10 - puzzle duplicate fixing

--- src/test/java/com/jcabi/w3c/DefaultCssValidatorTest.java
+++ src/test/java/com/jcabi/w3c/DefaultCssValidatorTest.java
@@ -92,7 +92,7 @@ public final class DefaultCssValidatorTest {
     /**
      * DefaultCssValidator throw IOException when W3C server error occurred.
      * @throws Exception If something goes wrong inside
-     * @todo #10:30min DefaultHtmlValidator have to be updated to throw only
+     * @todo #10:30min DefaultCssValidator have to be updated to throw only
      *  IOException when W3C validation server is unavailable. Any other
      *  exception type can be confusing for users. Remove @Ignore annotation
      *  after finishing implementation.
